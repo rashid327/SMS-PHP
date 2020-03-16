f45b1d88b417894dcfbd4375c8d2406a217be801
Rashid ALi <49899864+rashid327@users.noreply.github.com>
2020-03-16 14:39:07 +0500

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,7 +3,6 @@ require('SendSMS.php');
 
 $sendsms = new SendSMS("admin@gmail.com","*****","iPOpQbiUTD");
 
-
 /* if the login return 0, means that login failed, you cant send sms after this */
 if  (  ($sendsms->login()) == 0  ) {
 	die( "failed");
@@ -12,6 +11,5 @@ if  (  ($sendsms->login()) == 0  ) {
 /*other wise, you can send sms using the simple send() call*/
 
 $sendsms->send("+92123456789","please update me, when you receive any message?");
-
 
 ?>
