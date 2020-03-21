dfd74ef19c5aa2d7ca6071971a359c1123b20182
Rashid ALi <49899864+rashid327@users.noreply.github.com>
2020-03-21 10:20:17 -0700

    Update SendSMS.php

--- SendSMS.php
+++ SendSMS.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 /*
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -41,7 +39,6 @@ class SendSMS {
 		$this->api = $api;
 
 	}
-
 
 	public function login () {
 		$ch = curl_init();
